81e8de5722322f731c74b3419484358185234403
Jasurbek Abdurakhmonov <jaska111@bk.ru>
2023-05-24 03:03:30 +0500

    fix

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
 import * as dotenv from 'dotenv' 
+import axios from 'axios' 
+
 dotenv.config()
 
 async function sendMessageToTelegram(message) {
   const telegramBotToken = process.env.TG_TOKEN;
   const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
-
-  try {
-    await fetch(telegramApiUrl, {
-      method: 'POST',
+    return axios.post(telegramApiUrl, 
+        { 
+          chat_id: process.env.CHAT_ID,
+          text: message,
+        }, {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        chat_id: process.env.CHAT_ID,
-        text: message,
-      }),
+    }).then(res => {
+        console.log('Message sent to Telegram');
     });
-    console.log('Message sent to Telegram');
-  } catch (error) {
-    console.error('Failed to send message to Telegram:', error);
-  }
+  
 }
 
 async function apiCheck() {
     const car  = process.env.CAR
     console.debug('Run.........')
-  try {
-    const response = await fetch("http://savdo.uzavtosanoat.uz/b/ap/stock_list&models_all_post", {
+   return axios.post("http://savdo.uzavtosanoat.uz/b/ap/stock_list&models_all_post", { "referrer": "http://savdo.uzavtosanoat.uz/",
+    "filial_id": 100
+    }, {
         "headers": {
           "accept": "application/json, text/plain, */*",
           "accept-language": "en-US,en;q=0.9,fr;q=0.8,ru;q=0.7",
           "content-type": "application/json;charset=UTF-8",
           "lang_code": "ru"
         },
-        "referrer": "http://savdo.uzavtosanoat.uz/",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": "{\"filial_id\":100}",
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-      });
-    const data = await response.json();
-    if(data && Array.isArray(data)){
-        for(const d of data){
-            console.log(d?.name)
-            if(d?.name.includes(car) || d.name.includes(car.toLowerCase())){
-                sendMessageToTelegram(`${car} sale started`);
+      })
+      .then(response => {
+        const {data} = response
+        if(data && Array.isArray(data)){
+            for(const d of data){
+                console.log(d?.name)
+                if(d?.name.includes(car) || d.name.includes(car.toLowerCase())){
+                    sendMessageToTelegram(`${car} sale started`);
+                }
             }
         }
-    }
-  } catch (error) {
-    console.error('Failed to fetch:', error);
-  }
+      })
+      .catch(error => {
+        console.error('Failed to fetch: ', error);
+      });
+
 }
 
 setInterval(apiCheck, 10000);
 
 
+
+
+fetch("http://savdo.uzavtosanoat.uz/b/ap/stock_list&models_all_post", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9,fr;q=0.8,ru;q=0.7",
+    "content-type": "application/json;charset=UTF-8",
+    "lang_code": "ru",
+    "cookie": "lang=ru; JSESSIONID=31A7BB3E0CE787A9B39D86703CDA72D4",
+    "Referer": "http://savdo.uzavtosanoat.uz/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"filial_id\":100}",
+  "method": "POST"
+});
